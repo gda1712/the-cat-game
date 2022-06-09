503c2e788ee7fc523d68c89d84dbe972b41c012e
Gabriel Díaz Andrade <gda.m1712@gmail.com>
2022-06-09 02:15:31 -0400

    Add mov in the cat

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -9,10 +9,17 @@ public class Asteroid : MonoBehaviour
     private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
     private SpriteRenderer spriteRenderer;
 
+    public bool isDestroyed;
+    //public GameObject gameObject;
+    
+    // this is the position in the array x, y
+    public Vector2 actualBoardCoordinatesPosition;
+
     private void Awake()
     {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        this.isDestroyed = false;
+
         //throw new NotImplementedException();
     }
 
@@ -25,6 +32,13 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(this.actualBoardCoordinatesPosition);
+        this.isDestroyed = true;
+        Destroy(this.spriteRenderer);
     }
 }
--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -7,8 +7,13 @@ public class Cat : MonoBehaviour
     
     public static Cat sharedInstance;
     private static SpriteRenderer spriteRenderer;
+    public static GameObject gameObject;
 
-    public static Vector3 actualPosition;
+    // this is the position in the screen
+    public static Vector3 actualScreenPosition;
+
+    // this is the position in the array x, y
+    public static Vector2 actualBoardCoordinatesPosition;
     // Start is called before the first frame update
     void Start()
     {
