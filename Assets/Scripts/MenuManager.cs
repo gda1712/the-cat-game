10e767dc72b07ac9bff8147af40e2d1170b50278
Pedro Soto <pedrosotto717@gmail.com>
2022-06-10 07:05:54 -0400

    [add] Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void PlayButton()
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    // Start is called before the first frame update
+    public void PlayButton()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
+    public void QuitMenuButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
     // Update is called once per frame
-    void QuitButton()
+    public void QuitButton()
     {
         Debug.Log("CLosed App");
         Application.Quit();
